bc90182e492cf8dc1bdebc4874deefce0e9b9966
XJINE <xjine.andorelseif@gmail.com>
2016-10-10 23:46:45 +0900

    First Commit.

---
+++ Assets/OscPortSocket_Receiver.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Osc;
+
+public class OscPortSocket_Receiver : MonoBehaviour
+{
+    /// <summary>
+    /// OscPortSocket がメッセージを受信したときに呼び出されます。
+    /// OscPortSocket の OnReceive に設定します。
+    /// </summary>
+    /// <param name="e">
+    /// 受信したときに通知されるイベント。
+    /// </param>
+    public virtual void OscMessageReceived(OscPort.Capsule e)
+    {
+        Debug.Log("IP   : "   + e.ip);
+        Debug.Log("Path : " + e.message.path);
+
+        int length = e.message.data.Length;
+
+        for(int i = 0; i < length; i++)
+        {
+            Debug.Log("Data[" + i + "] : " + e.message.data[i]);
+        }
+    }
+}
---
+++ Assets/OscPortSocket_Server.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Osc;
+using System.Net;
+
+public class OscPortSocket_Server : MonoBehaviour
+{
+    /// <summary>
+    /// 送信サーバー。
+    /// </summary>
+    public OscPort server;
+
+    protected virtual void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SendOsc();
+            Debug.Log("Send");
+        }
+    }
+
+    /// <summary>
+    /// OSC を送信します。
+    /// </summary>
+    private void SendOsc()
+    {
+        MessageEncoder oscEnc = new MessageEncoder("/path");
+
+        oscEnc.Add(3.14f);
+        oscEnc.Add(12345);
+
+        this.server.Send(oscEnc.Encode());
+
+        Debug.Log("Local Port : " + this.server.localPort);
+        Debug.Log("Host : " + this.server.defaultRemoteHost);
+    }
+}
